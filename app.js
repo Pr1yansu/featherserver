79946c07c06052d8cbe29345bc5c37ecc04a6045
Pr1yansu <113017699+Pr1yansu@users.noreply.github.com>
2023-07-22 13:52:59 +0530

    update

--- app.js
+++ app.js
@@ -30,7 +30,7 @@ app.use(passport.session());
 app.use(
   cors({
     credentials: true,
-    origin: "http://localhost:3000",
+    origin: "https://localhost:3000",
     methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
   })
 );
