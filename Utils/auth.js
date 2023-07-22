85391f0cf91906b54be0666a176fece029cd1e02
Pr1yansu <113017699+Pr1yansu@users.noreply.github.com>
2023-07-22 13:37:47 +0530

    update

--- Utils/auth.js
+++ Utils/auth.js
@@ -1,15 +1,13 @@
 const { ErrorHandler } = require("./errohandler");
 
 exports.isAuthenticated = (req, res, next) => {
-  const token = req.cookies;
-  if (token) {
+  if (req.isAuthenticated()) {
     return next();
   }
   return next(
-    new ErrorHandler(403, "You are not logged to access this resource")
+    new ErrorHandler(403, "You are not logged in to access this resource")
   );
 };
-
 exports.isAdmin = (req, res, next) => {
   if (req.user && req.user.role === "admin") {
     return next();
